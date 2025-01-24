d63d487705c364e5e54c5940c16950f49cc1e0c4
demian35 <josedemian@ciencias.unam.mx>
2025-01-23 19:25:09 -0600

    Class that representanting the model of a drink in the api

---
+++ APICoctails/Models/Drink.cs
@@ -0,0 +1,9 @@
+//class that modelating a Drink from de TheCocktailDB to the proyect
+public class Drink{
+    public string Name{get; set;} //name of the coctail
+    public string Category{get; set;} //category of the drink
+    public string InstructionsEng{get; set;}//intructions for the preparation of the drink
+    public string InstructionsEsp{get; set;}//instruccions for the preparations of the drink in spanish
+    public List<string> Ingredients{get; set;}//The ingredients of the drink will storage in a list of strings
+
+}
