0ea5ae5aff77dfed71a664cbf116f08b1246aa42
demian35 <josedemian@ciencias.unam.mx>
2025-01-24 15:41:47 -0600

    Model class Finished

--- APICoctails/Models/Drink.cs
+++ APICoctails/Models/Drink.cs
@@ -4,6 +4,6 @@ public class Drink{
     public string Category{get; set;} //category of the drink
     public string InstructionsEng{get; set;}//intructions for the preparation of the drink
     public string InstructionsEsp{get; set;}//instruccions for the preparations of the drink in spanish
-    public List<string> Ingredients{get; set;}//The ingredients of the drink will storage in a list of strings
+    public List<string> Ingredients{get; set;} //The ingredients of the drink will storage in a list of strings
 
 }
